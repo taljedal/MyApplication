0c8be0faaa089f83dc062001945ebc2b8e634241
taljedal <henrik@taljedal.se>
2015-04-22 16:30:33 +0200

    hej

--- app/src/main/java/com/example/henriktaljedal/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/henriktaljedal/myapplication/MapsActivity.java
@@ -1,27 +1,16 @@
 package com.example.henriktaljedal.myapplication;
 
-import android.app.Dialog;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONObject;
 
@@ -37,10 +26,10 @@ import java.util.Random;
 
 public class MapsActivity extends FragmentActivity implements LocationListener{
 
-    GoogleMap mGoogleMap; // Might be null if Google Play services APK is not available.
+
     double mLatitude=0;
     double mLongitude=0;
-    SupportMapFragment fragment = null;
+
 
 
 
@@ -57,50 +46,6 @@ public class MapsActivity extends FragmentActivity implements LocationListener{
 
 
 
-            setContentView(R.layout.activity_maps);
-            int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
-
-                // Getting Google Play availability status
-
-                if (status != ConnectionResult.SUCCESS) { // Google Play Services are not available
-
-                    int requestCode = 10;
-                    Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
-                    dialog.show();
-
-                } else { // Google Play Services are available
-
-                    // Getting reference to the SupportMapFragment
-                    fragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-
-
-                    // Getting Google Map
-                    mGoogleMap = fragment.getMap();
-
-                    // Enabling MyLocation in Google Map
-                    mGoogleMap.setMyLocationEnabled(true);
-                    LatLng myPlace = new LatLng(mLatitude, mLongitude);
-                    mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myPlace, 13));
-
-
-                    // Getting LocationManager object from System Service LOCATION_SERVICE
-                    LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-                    // Creating a criteria object to retrieve provider
-                    Criteria criteria = new Criteria();
-
-                    // Getting the name of the best provider
-                    String provider = locationManager.getBestProvider(criteria, true);
-
-                    // Getting Current Location From GPS
-                    Location location = locationManager.getLastKnownLocation(provider);
-
-                    if (location != null) {
-                        onLocationChanged(location);
-                    }
-
-                    locationManager.requestLocationUpdates(provider, 20000, 0, this);
-                }
 
 
                 StringBuilder sb = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
@@ -118,23 +63,14 @@ public class MapsActivity extends FragmentActivity implements LocationListener{
 
                 placesTask.execute(sb.toString());
 
-                Button btnReturn = (Button) findViewById(R.id.btn_return);
 
 
-        
-        btnReturn.setOnClickListener(new View.OnClickListener() {
 
-                    @Override
-                    public void onClick(View v) {
-                        start();
-                    }
-                });
+
+
 
 
     }
-
-
-
 
 
     /** A class, to download Google Places */
@@ -234,17 +170,14 @@ public class MapsActivity extends FragmentActivity implements LocationListener{
 
         protected void onPostExecute(List<HashMap<String,String>> list){
 
-            // Clears all the existing markers
-            mGoogleMap.clear();
 
-            //for(int i=0;i<list.size();i++){
+
+
 
                 Random rand = new Random();
 
                 int  i = rand.nextInt(list.size()-1);
 
-                // Creating a marker
-                MarkerOptions markerOptions = new MarkerOptions();
 
                 // Getting a place from the places list
                 HashMap<String, String> hmPlace = list.get(i);
@@ -263,38 +196,15 @@ public class MapsActivity extends FragmentActivity implements LocationListener{
 
                 LatLng latLng = new LatLng(lat, lng);
 
-                // Setting the position for the marker
-                markerOptions.position(latLng);
 
-                // Setting the title for the marker.
-                //This will be displayed on taping the marker
-                markerOptions.title(name + ": " + vicinity);
 
-                // Placing a marker on the touched position
-                mGoogleMap.addMarker(markerOptions);
-          //  }
         }
     }
 
 
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_maps, menu);
-        return true;
-    }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        mLatitude = location.getLatitude();
-        mLongitude = location.getLongitude();
-        LatLng latLng = new LatLng(mLatitude, mLongitude);
-
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(12));
-    }
     protected void start() {
         setContentView(R.layout.activity_main);
 
@@ -334,20 +244,28 @@ public class MapsActivity extends FragmentActivity implements LocationListener{
 
     }
 
-
     @Override
-    public void onProviderDisabled(String provider) {
-        // TODO Auto-generated method stub
+    public void onLocationChanged(Location location) {
+        mLatitude = location.getLatitude();
+        mLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(mLatitude, mLongitude);
+
+
+    }
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-        // TODO Auto-generated method stub
+
     }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        // TODO Auto-generated method stub
-    }
-}
+    public void onProviderDisabled(String provider) {
 
+    }
+
+
+}
